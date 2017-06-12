114076b692155eb56e1c623913040635298e0d8f
Student12 <Student12@Student12s-iMac.local>
2017-06-12 14:23:27 +0700

    Variable

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This Comment หรือ นี่คือ comment
+// นี้คือการประกาศตัวแปร แบบ ไม่สามารถเปลี่ยนค่าได้
+let myConstant = 123
+
+var intNumber = 123
