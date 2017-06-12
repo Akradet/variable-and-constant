f4c3bff55a77ad01b42eec0ad338e0c59b967898
Student12 <Student12@Student12s-iMac.local>
2017-06-12 14:55:12 +0700

    print

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -4,6 +4,17 @@ import UIKit
 
 // This Comment หรือ นี่คือ comment
 // นี้คือการประกาศตัวแปร แบบ ไม่สามารถเปลี่ยนค่าได้
-let myConstant = 123
+let myConstant = 123 // Imlicit constant
+//myConstant = 456 cannot change let
 
-var intNumber = 123
+//นี้คือก่ีประกาศตัวแปรที่เปลี่ยนค่าได้
+var intNumber = 123  //Implicit variable intNumber = 123
+intNumber = 456
+
+let myConstant1: Int = 123
+
+
+
+//Display on Console
+print(intNumber) // Display only Value
+print("intNumber ==> \")
