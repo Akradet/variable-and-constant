ebed0f2de09bcc9fa159214e046d0d958811b614
Student12 <Student12@Student12s-iMac.local>
2017-06-12 16:15:44 +0700

    print

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -17,4 +17,49 @@ let myConstant1: Int = 123
 
 //Display on Console
 print(intNumber) // Display only Value
-print("intNumber ==> \")
+print("intNumber ==> \(intNumber)")
+
+//Explicit String การประกาศแบบ กำหนด
+//Data Type ==> String
+let strName: String = "Doramon"
+
+//Implicit String Datatype ==> String
+let strSurname = "Japane"
+
+//Operate
+var strOfficer = strName + " " + strSurname
+strOfficer = "Mr." + strOfficer
+
+//Operating on Number
+let intA = 5
+let intB = 2
+
+var answer = intA + intB
+answer = intA * intB
+answer = intA / intB
+
+// Change Datatype Integer to Double
+var douA = Double(intA)
+var douB = Double(intB)
+
+var answer2: Double = douA / douB
+
+var myStatus = true //Implicit
+var myStatus2: =Bool = false //Explicit
+
+//print on Console ==> ถ้าเอา 5 บาท 6 เท่ากับ  11
+
+//String Manage
+let strTitle1 = "ถ้าเอา"
+let strTitle2 = "เท่ากับ"
+let strAdd = "บวก"
+let strHand = "หาร"
+let strSpace = " "
+
+//Int Manage
+let intNum1 = 5
+let intNum2 = 6
+
+print(strTitle1 + strSpace + "\(intNum1)" + strSpace + strAdd + strSpace + "\(intNum2)" + strSpace + strTitle2 + strSpace + "\(intNum1 + intNum2)" )
+
+
